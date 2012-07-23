airekans/chromium

commit e131d3546ef2ea4e3dee6304acf0644fda98c9d6

Aura: Have ui::Layer implement WebKit::WebExternalTextureLayerClient
With --ui-enable-threaded-compositing, every Layer::SetExternalTexture() was causing a heavy CCThreadProxy::acquireLayerTextures(). This synchronization is unnecessary on the common case of buffer flips, because RWHVA handles the necessary synchronization via OnCompositingWillStart callbacks.

To reduce the synchronization burden, we handle
- wait for a commit on resize fast ACKS
- handle the common case of buffer flips via WebExternalTextureLayerClient::prepareTexture()
- and explicitly call WebExternalTextureLayer::willModifyTexture() when we need the extra synchronization on tear down.

BUG=136012
TEST=none

Review URL: https://chromiumcodereview.appspot.com/10689108

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146621 0039d316-1c4b-4281-b951-d872f2087c98

gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -8233,11 +8233,6 @@ error::Error GLES2DecoderImpl::HandleSwapBuffers(
   }
 
   int this_frame_number = frame_number_++;
-  // TRACE_EVENT for gpu tests:
-  TRACE_EVENT_INSTANT2("test_gpu", "SwapBuffers",
-                       "GLImpl", static_cast<int>(gfx::GetGLImplementation()),
-                       "width", (is_offscreen ? offscreen_size_.width() :
-                                 surface_->GetSize().width()));
   TRACE_EVENT2("gpu", "GLES2DecoderImpl::HandleSwapBuffers",
                "offscreen", is_offscreen,
                "frame", this_frame_number);

ash/wm/window_animations.cc
@@ -566,6 +566,8 @@ class CrossFadeObserver : public ui::CompositorObserver,
   }
 
   // ui::CompositorObserver overrides:
+  virtual void OnCompositingDidCommit(ui::Compositor* compositor) OVERRIDE {
+  }
   virtual void OnCompositingWillStart(ui::Compositor* compositor) OVERRIDE {
   }
   virtual void OnCompositingStarted(ui::Compositor* compositor) OVERRIDE {

chrome/test/gpu/gpu_feature_browsertest.cc
@@ -22,6 +22,7 @@
 #include "content/test/gpu/test_switches.h"
 #include "net/base/net_util.h"
 #include "ui/gl/gl_switches.h"
+#include "ui/compositor/compositor_setup.h"
 #if defined(OS_MACOSX)
 #include "ui/surface/io_surface_support_mac.h"
 #endif
@@ -66,6 +67,7 @@ class GpuFeatureTest : public InProcessBrowserTest {
       gpu_enabled_ = true;
     }
     command_line->AppendSwitch(switches::kDisablePopupBlocking);
+    ui::DisableTestCompositor();
     command_line->AppendSwitchASCII(switches::kWindowSize, "400,300");
   }
 

content/browser/renderer_host/image_transport_client.cc
@@ -1,9 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "content/browser/renderer_host/image_transport_client.h"
-
-ImageTransportClient::ImageTransportClient(bool flipped, const gfx::Size& size)
-    : ui::Texture(flipped, size) {
-}

content/browser/renderer_host/image_transport_client.h
@@ -1,46 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#ifndef CONTENT_BROWSER_RENDERER_HOST_IMAGE_TRANSPORT_CLIENT_H_
-#define CONTENT_BROWSER_RENDERER_HOST_IMAGE_TRANSPORT_CLIENT_H_
-
-#include "base/basictypes.h"
-#include "ui/compositor/compositor.h"
-#include "ui/surface/transport_dib.h"
-
-namespace gfx {
-class Size;
-}
-class ImageTransportFactory;
-
-// This is a client for ImageTransportSurface, that handles the
-// platform-specific task of binding the transport surface to a GL texture.
-// The GL texture is allocated in the ImageTransportFactory context, and the
-// data is only valid after the first Update().
-class ImageTransportClient : public ui::Texture {
- public:
-  // Initializes the client with the surface id.
-  virtual bool Initialize(uint64* surface_handle) = 0;
-
-  // Updates the surface data into the texture.
-  virtual void Update() = 0;
-
-  // Returns the shared memory handle used to transfer software data if needed.
-  // Can be a NULL handle.
-  virtual TransportDIB::Handle Handle() const = 0;
-
-  // Creates a platform-specific client.
-  static ImageTransportClient* Create(ImageTransportFactory* factory,
-                                      const gfx::Size& size);
-
- protected:
-  ImageTransportClient(bool flipped, const gfx::Size& size);
-  virtual ~ImageTransportClient() {}
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(ImageTransportClient);
-};
-
-#endif  // CONTENT_BROWSER_RENDERER_HOST_IMAGE_TRANSPORT_CLIENT_H_
-

content/browser/renderer_host/image_transport_client_linux.cc
@@ -1,311 +1,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "content/browser/renderer_host/image_transport_client.h"
-
-#include <X11/Xlib.h>
-#include <X11/extensions/Xcomposite.h>
-
-#include "base/debug/trace_event.h"
-#include "base/lazy_instance.h"
-#include "base/memory/scoped_ptr.h"
-#include "base/message_loop.h"
-#include "content/browser/renderer_host/image_transport_factory.h"
-#include "third_party/angle/include/EGL/egl.h"
-#include "third_party/angle/include/EGL/eglext.h"
-#include "ui/gfx/size.h"
-#include "ui/gl/gl_bindings.h"
-#include "ui/gl/gl_implementation.h"
-#include "ui/gl/gl_surface_egl.h"
-#include "ui/gl/gl_surface_glx.h"
-#include "ui/gl/scoped_make_current.h"
-
-namespace {
-
-class ScopedPtrXFree {
- public:
-  void operator()(void* x) const {
-    ::XFree(x);
-  }
-};
-
-GLuint CreateTexture() {
-  GLuint texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture);
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  return texture;
-}
-
-class ImageTransportClientEGL : public ImageTransportClient {
- public:
-  ImageTransportClientEGL(ImageTransportFactory* factory, const gfx::Size& size)
-      : ImageTransportClient(true, size),
-        factory_(factory),
-        image_(NULL) {
-  }
-
-  virtual bool Initialize(uint64* surface_handle) OVERRIDE {
-    scoped_ptr<gfx::ScopedMakeCurrent> bind(factory_->GetScopedMakeCurrent());
-    image_ = eglCreateImageKHR(
-        gfx::GLSurfaceEGL::GetHardwareDisplay(), EGL_NO_CONTEXT,
-        EGL_NATIVE_PIXMAP_KHR, reinterpret_cast<void*>(*surface_handle), NULL);
-    if (!image_)
-      return false;
-    set_texture_id(CreateTexture());
-    glBindTexture(GL_TEXTURE_2D, texture_id());
-    glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, image_);
-    glFlush();
-    return true;
-  }
-
-  virtual void Update() OVERRIDE {}
-  virtual TransportDIB::Handle Handle() const OVERRIDE {
-    return TransportDIB::DefaultHandleValue();
-  }
-
- protected:
-  virtual ~ImageTransportClientEGL() {
-    scoped_ptr<gfx::ScopedMakeCurrent> bind(factory_->GetScopedMakeCurrent());
-    if (image_)
-      eglDestroyImageKHR(gfx::GLSurfaceEGL::GetHardwareDisplay(), image_);
-    unsigned int texture = texture_id();
-    if (texture)
-      glDeleteTextures(1, &texture);
-    glFlush();
-  }
-
- private:
-  ImageTransportFactory* factory_;
-  EGLImageKHR image_;
-};
-
-class ImageTransportClientGLX : public ImageTransportClient {
- public:
-  ImageTransportClientGLX(ImageTransportFactory* factory, const gfx::Size& size)
-      : ImageTransportClient(false, size),
-        factory_(factory),
-        pixmap_(0),
-        glx_pixmap_(0),
-        acquired_(false) {
-  }
-
-  virtual bool Initialize(uint64* surface_handle) OVERRIDE {
-    TRACE_EVENT0("renderer_host", "ImageTransportClientGLX::Initialize");
-    Display* dpy = base::MessagePumpForUI::GetDefaultXDisplay();
-
-    scoped_ptr<gfx::ScopedMakeCurrent> bind(factory_->GetScopedMakeCurrent());
-    if (!InitializeOneOff(dpy))
-      return false;
-
-    // Create pixmap from window.
-    // We receive a window here rather than a pixmap directly because drivers
-    // require (or required) that the pixmap used to create the GL texture be
-    // created in the same process as the texture.
-    pixmap_ = XCompositeNameWindowPixmap(dpy, *surface_handle);
-
-    const int pixmapAttribs[] = {
-      GLX_TEXTURE_TARGET_EXT, GLX_TEXTURE_2D_EXT,
-      GLX_TEXTURE_FORMAT_EXT, GLX_TEXTURE_FORMAT_RGB_EXT,
-      0
-    };
-
-    glx_pixmap_ = glXCreatePixmap(dpy, fbconfig_.Get(), pixmap_, pixmapAttribs);
-
-    set_texture_id(CreateTexture());
-    glFlush();
-    return true;
-  }
-
-  virtual void Update() OVERRIDE {
-    TRACE_EVENT0("renderer_host", "ImageTransportClientGLX::Update");
-    Display* dpy = base::MessagePumpForUI::GetDefaultXDisplay();
-    scoped_ptr<gfx::ScopedMakeCurrent> bind(factory_->GetScopedMakeCurrent());
-    glBindTexture(GL_TEXTURE_2D, texture_id());
-    if (acquired_)
-      glXReleaseTexImageEXT(dpy, glx_pixmap_, GLX_FRONT_LEFT_EXT);
-    glXBindTexImageEXT(dpy, glx_pixmap_, GLX_FRONT_LEFT_EXT, NULL);
-    acquired_ = true;
-    glFlush();
-  }
-
-  virtual TransportDIB::Handle Handle() const OVERRIDE {
-    return TransportDIB::DefaultHandleValue();
-  }
-
- protected:
-  virtual ~ImageTransportClientGLX() {
-    scoped_ptr<gfx::ScopedMakeCurrent> bind(factory_->GetScopedMakeCurrent());
-    Display* dpy = base::MessagePumpForUI::GetDefaultXDisplay();
-    if (glx_pixmap_) {
-      if (acquired_)
-        glXReleaseTexImageEXT(dpy, glx_pixmap_, GLX_FRONT_LEFT_EXT);
-      glXDestroyGLXPixmap(dpy, glx_pixmap_);
-    }
-    if (pixmap_)
-      XFreePixmap(dpy, pixmap_);
-    unsigned int texture = texture_id();
-    if (texture)
-      glDeleteTextures(1, &texture);
-    glFlush();
-  }
-
- private:
-  static bool InitializeOneOff(Display* dpy) {
-    static bool initialized = false;
-    if (initialized)
-      return true;
-
-    int event_base, error_base;
-    if (XCompositeQueryExtension(dpy, &event_base, &error_base)) {
-      int major = 0, minor = 2;
-      XCompositeQueryVersion(dpy, &major, &minor);
-      if (major == 0 && minor < 2) {
-        LOG(ERROR) << "Pixmap from window not supported.";
-        return false;
-      }
-    }
-    // Wrap the pixmap in a GLXPixmap
-    int screen = DefaultScreen(dpy);
-    XWindowAttributes gwa;
-    XGetWindowAttributes(dpy, RootWindow(dpy, screen), &gwa);
-    unsigned int visualid = XVisualIDFromVisual(gwa.visual);
-
-    int nfbconfigs, config;
-    scoped_ptr_malloc<GLXFBConfig, ScopedPtrXFree> fbconfigs(
-        glXGetFBConfigs(dpy, screen, &nfbconfigs));
-
-    for (config = 0; config < nfbconfigs; config++) {
-      XVisualInfo* visinfo = glXGetVisualFromFBConfig(
-          dpy, fbconfigs.get()[config]);
-      if (!visinfo || visinfo->visualid != visualid)
-        continue;
-
-      int value;
-      glXGetFBConfigAttrib(dpy,
-          fbconfigs.get()[config],
-          GLX_DRAWABLE_TYPE,
-          &value);
-      if (!(value & GLX_PIXMAP_BIT))
-        continue;
-
-      glXGetFBConfigAttrib(dpy,
-          fbconfigs.get()[config],
-          GLX_BIND_TO_TEXTURE_TARGETS_EXT,
-          &value);
-      if (!(value & GLX_TEXTURE_2D_BIT_EXT))
-        continue;
-
-      glXGetFBConfigAttrib(dpy,
-          fbconfigs.get()[config],
-          GLX_BIND_TO_TEXTURE_RGB_EXT,
-          &value);
-      if (value == GL_FALSE)
-        continue;
-
-      break;
-    }
-
-    if (config == nfbconfigs) {
-      LOG(ERROR)
-        << "Could not find configuration suitable for binding a pixmap "
-        << "as a texture.";
-      return false;
-    }
-    fbconfig_.Get() = fbconfigs.get()[config];
-    initialized = true;
-    return initialized;
-  }
-
-  ImageTransportFactory* factory_;
-  XID pixmap_;
-  XID glx_pixmap_;
-  bool acquired_;
-  static base::LazyInstance<GLXFBConfig> fbconfig_;
-};
-
-base::LazyInstance<GLXFBConfig> ImageTransportClientGLX::fbconfig_ =
-    LAZY_INSTANCE_INITIALIZER;
-
-class ImageTransportClientOSMesa : public ImageTransportClient {
- public:
-  ImageTransportClientOSMesa(ImageTransportFactory* factory,
-                             const gfx::Size& size)
-      : ImageTransportClient(false, size),
-        factory_(factory) {
-  }
-
-  virtual bool Initialize(uint64* surface_handle) OVERRIDE {
-    // We expect to make the handle here, so don't want the other end giving us
-    // one.
-    DCHECK_EQ(*surface_handle, static_cast<uint64>(0));
-
-    // It's possible that this ID gneration could clash with IDs from other
-    // AcceleratedSurfaceContainerTouch* objects, however we should never have
-    // ids active from more than one type at the same time, so we have free
-    // reign of the id namespace.
-    *surface_handle = next_handle_++;
-
-    shared_mem_.reset(
-        TransportDIB::Create(size().GetArea() * 4,  // GL_RGBA=4 B/px
-        *surface_handle));
-    if (!shared_mem_.get())
-      return false;
-
-    scoped_ptr<gfx::ScopedMakeCurrent> bind(factory_->GetScopedMakeCurrent());
-    set_texture_id(CreateTexture());
-    glFlush();
-    return true;
-  }
-
-  virtual void Update() OVERRIDE {
-    glBindTexture(GL_TEXTURE_2D, texture_id());
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
-                 size().width(), size().height(), 0,
-                 GL_RGBA, GL_UNSIGNED_BYTE, shared_mem_->memory());
-    glFlush();
-  }
-
-  virtual TransportDIB::Handle Handle() const OVERRIDE {
-    return shared_mem_->handle();
-  }
-
- protected:
-  virtual ~ImageTransportClientOSMesa() {
-    unsigned int texture = texture_id();
-    if (texture) {
-      scoped_ptr<gfx::ScopedMakeCurrent> bind(
-          factory_->GetScopedMakeCurrent());
-      glDeleteTextures(1, &texture);
-      glFlush();
-    }
-  }
-
- private:
-  ImageTransportFactory* factory_;
-  scoped_ptr<TransportDIB> shared_mem_;
-  static uint32 next_handle_;
-};
-uint32 ImageTransportClientOSMesa::next_handle_ = 0;
-
-}  // anonymous namespace
-
-ImageTransportClient* ImageTransportClient::Create(
-    ImageTransportFactory* factory,
-    const gfx::Size& size) {
-  switch (gfx::GetGLImplementation()) {
-    case gfx::kGLImplementationOSMesaGL:
-      return new ImageTransportClientOSMesa(factory, size);
-    case gfx::kGLImplementationDesktopGL:
-      return new ImageTransportClientGLX(factory, size);
-    case gfx::kGLImplementationEGLGLES2:
-      return new ImageTransportClientEGL(factory, size);
-    default:
-      NOTREACHED();
-      return NULL;
-  }
-}

content/browser/renderer_host/image_transport_factory.cc
@@ -13,7 +13,6 @@
 #include "base/observer_list.h"
 #include "content/browser/gpu/browser_gpu_channel_host_factory.h"
 #include "content/browser/gpu/gpu_surface_tracker.h"
-#include "content/browser/renderer_host/image_transport_client.h"
 #include "content/common/gpu/client/gl_helper.h"
 #include "content/common/gpu/client/gpu_channel_host.h"
 #include "content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h"
@@ -23,6 +22,7 @@
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebGraphicsContext3D.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/compositor_setup.h"
+#include "ui/compositor/test_web_graphics_context_3d.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/size.h"
 #include "ui/gl/scoped_make_current.h"
@@ -55,9 +55,10 @@ class DefaultTransportFactory
       gfx::GLSurfaceHandle surface) OVERRIDE {
   }
 
-  virtual scoped_refptr<ImageTransportClient> CreateTransportClient(
+  virtual scoped_refptr<ui::Texture> CreateTransportClient(
       const gfx::Size& size,
-      uint64* transport_handle) OVERRIDE {
+      uint64* transport_handle,
+      ui::Compositor* compositor) OVERRIDE {
     return NULL;
   }
 
@@ -67,10 +68,6 @@ class DefaultTransportFactory
 
   virtual uint32 InsertSyncPoint(ui::Compositor* compositor) OVERRIDE {
     return 0;
-  }
-
-  virtual gfx::ScopedMakeCurrent* GetScopedMakeCurrent() OVERRIDE {
-    return NULL;
   }
 
   // We don't generate lost context events, so we don't need to keep track of
@@ -86,55 +83,30 @@ class DefaultTransportFactory
   DISALLOW_COPY_AND_ASSIGN(DefaultTransportFactory);
 };
 
-class TestTransportFactory : public DefaultTransportFactory {
+class ImageTransportClientTexture : public ui::Texture {
  public:
-  TestTransportFactory() {}
-
-  virtual gfx::GLSurfaceHandle CreateSharedSurfaceHandle(
-      ui::Compositor* compositor) OVERRIDE {
-    return gfx::GLSurfaceHandle(gfx::kNullPluginWindow, true);
-  }
-
-  virtual scoped_refptr<ImageTransportClient> CreateTransportClient(
+  ImageTransportClientTexture(
+      WebKit::WebGraphicsContext3D* host_context,
       const gfx::Size& size,
-      uint64* transport_handle) OVERRIDE {
-#if defined(UI_COMPOSITOR_IMAGE_TRANSPORT)
-    scoped_refptr<ImageTransportClient> surface(
-        ImageTransportClient::Create(this, size));
-    if (!surface || !surface->Initialize(transport_handle)) {
-      LOG(ERROR) << "Failed to create ImageTransportClient";
-      return NULL;
-    }
-    return surface;
-#else
-    return NULL;
-#endif
+      uint64 surface_id)
+          : ui::Texture(true, size),
+            host_context_(host_context) {
+    set_texture_id(surface_id);
   }
 
- private:
-  DISALLOW_COPY_AND_ASSIGN(TestTransportFactory);
-};
-
-class ImageTransportClientTexture : public ImageTransportClient {
- public:
-  explicit ImageTransportClientTexture(const gfx::Size& size)
-      : ImageTransportClient(true, size) {
-  }
-
-  virtual bool Initialize(uint64* surface_id) OVERRIDE {
-    set_texture_id(*surface_id);
-    return true;
-  }
-
-  virtual void Update() OVERRIDE {}
-  virtual TransportDIB::Handle Handle() const OVERRIDE {
-    return TransportDIB::DefaultHandleValue();
+  virtual WebKit::WebGraphicsContext3D* HostContext3D() {
+    return host_context_;
   }
 
  protected:
   virtual ~ImageTransportClientTexture() {}
 
  private:
+  // A raw pointer. This |ImageTransportClientTexture| will be destroyed
+  // before the |host_context_| via
+  // |ImageTransportFactoryObserver::OnLostContext()| handlers.
+  WebKit::WebGraphicsContext3D* host_context_;
+
   DISALLOW_COPY_AND_ASSIGN(ImageTransportClientTexture);
 };
 
@@ -247,12 +219,15 @@ class GpuProcessTransportFactory : public ui::ContextFactory,
     }
   }
 
-  virtual scoped_refptr<ImageTransportClient> CreateTransportClient(
+  virtual scoped_refptr<ui::Texture> CreateTransportClient(
       const gfx::Size& size,
-      uint64* transport_handle) {
+      uint64* transport_handle,
+      ui::Compositor* compositor) {
+    PerCompositorData* data = per_compositor_data_[compositor];
+    DCHECK(data);
     scoped_refptr<ImageTransportClientTexture> image(
-        new ImageTransportClientTexture(size));
-    image->Initialize(transport_handle);
+        new ImageTransportClientTexture(data->shared_context.get(),
+                                        size, *transport_handle));
     return image;
   }
 
@@ -277,8 +252,6 @@ class GpuProcessTransportFactory : public ui::ContextFactory,
       data = CreatePerCompositorData(compositor);
     return data->shared_context->insertSyncPoint();
   }
-
-  virtual gfx::ScopedMakeCurrent* GetScopedMakeCurrent() { return NULL; }
 
   virtual void AddObserver(ImageTransportFactoryObserver* observer) {
     observer_list_.AddObserver(observer);
@@ -412,7 +385,7 @@ void ImageTransportFactory::Initialize() {
     ui::SetupTestCompositor();
   }
   if (ui::IsTestCompositorEnabled()) {
-    g_factory = new TestTransportFactory();
+    g_factory = new DefaultTransportFactory();
   } else {
 #if defined(OS_WIN)
     g_factory = new DefaultTransportFactory();

content/browser/renderer_host/image_transport_factory.h
@@ -27,8 +27,6 @@ namespace WebKit {
 class WebGraphicsContext3D;
 }
 
-class ImageTransportClient;
-
 // This class provides a way to get notified when surface handles get lost.
 class ImageTransportFactoryObserver {
  public:
@@ -74,11 +72,12 @@ class ImageTransportFactory {
   // Destroys a shared surface handle.
   virtual void DestroySharedSurfaceHandle(gfx::GLSurfaceHandle surface) = 0;
 
-  // Creates a transport client of a given size, and using the opaque handle
+  // Creates a transport texture of a given size, and using the opaque handle
   // sent by the GPU process.
-  virtual scoped_refptr<ImageTransportClient> CreateTransportClient(
+  virtual scoped_refptr<ui::Texture> CreateTransportClient(
       const gfx::Size& size,
-      uint64* transport_handle) = 0;
+      uint64* transport_handle,
+      ui::Compositor* compositor) = 0;
 
   // Gets a GLHelper instance, associated with the compositor context. This
   // GLHelper will get destroyed whenever the context is lost (
@@ -86,12 +85,6 @@ class ImageTransportFactory {
 
   // Inserts a SyncPoint into the compositor's context.
   virtual uint32 InsertSyncPoint(ui::Compositor* compositor) = 0;
-
-  // Returns a ScopedMakeCurrent that can be used to make current a context that
-  // is shared with the compositor context, e.g. to create a texture in its
-  // namespace. The caller gets ownership of the object.
-  // This will return NULL when using out-of-process (command buffer) contexts.
-  virtual gfx::ScopedMakeCurrent* GetScopedMakeCurrent() = 0;
 
   virtual void AddObserver(ImageTransportFactoryObserver* observer) = 0;
   virtual void RemoveObserver(ImageTransportFactoryObserver* observer) = 0;

commit 27558babdf0c38976a1521c9cacc7857ad6fdfa0

GPU: Fix latency tests
Latency tests require instrumenting where the SwapBuffers call is received, as opposed to where it is completed. Fix a regression introduced by r146621 in the latency tests by restoring the TRACE_EVENT placement.

R=jbates
BUG=none
TEST=none


Review URL: https://chromiumcodereview.appspot.com/10805022

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147923 0039d316-1c4b-4281-b951-d872f2087c98

gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -8241,6 +8241,11 @@ error::Error GLES2DecoderImpl::HandleSwapBuffers(
   }
 
   int this_frame_number = frame_number_++;
+  // TRACE_EVENT for gpu tests:
+  TRACE_EVENT_INSTANT2("test_gpu", "SwapBuffersLatency",
+                       "GLImpl", static_cast<int>(gfx::GetGLImplementation()),
+                       "width", (is_offscreen ? offscreen_size_.width() :
+                                 surface_->GetSize().width()));
   TRACE_EVENT2("gpu", "GLES2DecoderImpl::HandleSwapBuffers",
                "offscreen", is_offscreen,
                "frame", this_frame_number);

chrome/test/perf/rendering/latency_tests.cc
@@ -272,7 +272,7 @@ void LatencyTest::RunTest(LatencyTestMode mode,
   // Construct queries for searching trace events via TraceAnalyzer.
   if (mode_ == kWebGL) {
     query_begin_swaps_ = query_instant_ &&
-        Query::EventName() == Query::String("SwapBuffers") &&
+        Query::EventName() == Query::String("SwapBuffersLantency") &&
         Query::EventArg("width") != Query::Int(kWebGLCanvasWidth);
     query_end_swaps_ = query_instant_ &&
         Query::EventName() == Query::String("CompositorSwapBuffersComplete");
@@ -626,7 +626,7 @@ void LatencyTest::PrintEvents(const TraceEventVector& events) {
     } else if (events[i]->name == "DoBlit") {
       // WebGL context swap buffers.
       printf("BLT ");
-    } else if (events[i]->name == "SwapBuffers") {
+    } else if (events[i]->name == "SwapBuffersLatency") {
       // Compositor context swap buffers.
       ++swap_count;
       printf("|\nframe %03d: ", swap_count + 1);