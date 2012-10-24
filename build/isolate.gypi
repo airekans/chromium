airekans/chromium

commit 5702cc9d73900c405eed21bf91b5daccf85efc5f

Convert all the foo_test_run from using actions to use rules.
This removes a lot of the duplication of the exact details
about how to run isolate.py exactly.. This will permit
inserting arbitrary command line arguments eventually, for
--subdir support.

TBR=csharp@chromium.org
BUG=


Review URL: https://chromiumcodereview.appspot.com/11236005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163144 0039d316-1c4b-4281-b951-d872f2087c98

build/isolate.gypi
@@ -1,0 +1,61 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This file is meant to be included into a target to provide a rule
+# to "build" .isolate files into a .isolated file.
+#
+# To use this, create a gyp target with the following form:
+# 'conditions': [
+#   ['test_isolation_mode != "noop"', {
+#     'targets': [
+#       {
+#         'target_name': 'foo_test_run',
+#         'type': 'none',
+#         'dependencies': [
+#           'foo_test',
+#         ],
+#         'includes': [
+#           '../build/isolate.gypi',
+#           'foo_test.isolate',
+#         ],
+#         'sources': [
+#           'foo_test.isolate',
+#         ],
+#       },
+#     ],
+#   }],
+# ],
+#
+# Note: foo_test.isolate is included and a source file. It is an inherent
+# property of the .isolate format. This permits to define GYP variables but is
+# a stricter format than GYP so isolate.py can read it.
+#
+# The generated .isolated file will be:
+#   <(PRODUCT_DIR)/foo_test.isolated
+
+{
+  'rules': [
+    {
+      'rule_name': 'isolate',
+      'extension': 'isolate',
+      'inputs': [
+        '<@(isolate_dependency_tracked)',
+        '<(RULE_INPUT_PATH)',
+      ],
+      'outputs': [
+        '<(PRODUCT_DIR)/<(RULE_INPUT_ROOT).isolated',
+      ],
+      'action': [
+        '<(DEPTH)/tools/swarm_client/isolate.py',
+        '<(test_isolation_mode)',
+        '--outdir', '<(test_isolation_outdir)',
+        '--variable', 'PRODUCT_DIR', '<(PRODUCT_DIR)',
+        '--variable', 'OS', '<(OS)',
+        '--result', '<@(_outputs)',
+        '--isolate', '<(RULE_INPUT_PATH)',
+      ],
+      'msvs_cygwin_shell': 0,
+    },
+  ],
+}

base/base.gyp
@@ -1052,29 +1052,11 @@
             'base_unittests',
           ],
           'includes': [
+            '../build/isolate.gypi',
             'base_unittests.isolate',
           ],
-          'actions': [
-            {
-              'action_name': 'isolate',
-              'inputs': [
-                'base_unittests.isolate',
-                '<@(isolate_dependency_tracked)',
-              ],
-              'outputs': [
-                '<(PRODUCT_DIR)/base_unittests.isolated',
-              ],
-              'action': [
-                'python',
-                '../tools/swarm_client/isolate.py',
-                '<(test_isolation_mode)',
-                '--outdir', '<(test_isolation_outdir)',
-                '--variable', 'PRODUCT_DIR', '<(PRODUCT_DIR)',
-                '--variable', 'OS', '<(OS)',
-                '--result', '<@(_outputs)',
-                '--isolate', 'base_unittests.isolate',
-              ],
-            },
+          'sources': [
+            'base_unittests.isolate',
           ],
         },
       ],

chrome/chrome_tests.gypi
@@ -4835,28 +4835,11 @@
             'unit_tests',
           ],
           'includes': [
+            '../build/isolate.gypi',
             'unit_tests.isolate',
           ],
-          'actions': [
-            {
-              'action_name': 'isolate',
-              'inputs': [
-                '<@(isolate_dependency_tracked)',
-              ],
-              'outputs': [
-                '<(PRODUCT_DIR)/unit_tests.isolated',
-              ],
-              'action': [
-                'python',
-                '../tools/swarm_client/isolate.py',
-                '<(test_isolation_mode)',
-                '--outdir', '<(test_isolation_outdir)',
-                '--variable', 'PRODUCT_DIR', '<(PRODUCT_DIR)',
-                '--variable', 'OS', '<(OS)',
-                '--result', '<@(_outputs)',
-                '--isolate', 'unit_tests.isolate',
-              ],
-            },
+          'sources': [
+            'unit_tests.isolate',
           ],
         },
         {
@@ -4866,28 +4849,11 @@
             'browser_tests',
           ],
           'includes': [
+            '../build/isolate.gypi',
             'browser_tests.isolate',
           ],
-          'actions': [
-            {
-              'action_name': 'isolate',
-              'inputs': [
-                '<@(isolate_dependency_tracked)',
-              ],
-              'outputs': [
-                '<(PRODUCT_DIR)/browser_tests.isolated',
-              ],
-              'action': [
-                'python',
-                '../tools/swarm_client/isolate.py',
-                '<(test_isolation_mode)',
-                '--outdir', '<(test_isolation_outdir)',
-                '--variable', 'PRODUCT_DIR', '<(PRODUCT_DIR)',
-                '--variable', 'OS', '<(OS)',
-                '--result', '<@(_outputs)',
-                '--isolate', 'browser_tests.isolate',
-              ],
-            },
+          'sources': [
+            'browser_tests.isolate',
           ],
         },
         {
@@ -4904,28 +4870,11 @@
             }],
           ],
           'includes': [
+            '../build/isolate.gypi',
             'sync_integration_tests.isolate',
           ],
-          'actions': [
-            {
-              'action_name': 'isolate',
-              'inputs': [
-                '<@(isolate_dependency_tracked)',
-              ],
-              'outputs': [
-                '<(PRODUCT_DIR)/sync_integration_tests.isolated',
-              ],
-              'action': [
-                'python',
-                '../tools/swarm_client/isolate.py',
-                '<(test_isolation_mode)',
-                '--outdir', '<(test_isolation_outdir)',
-                '--variable', 'PRODUCT_DIR', '<(PRODUCT_DIR)',
-                '--variable', 'OS', '<(OS)',
-                '--result', '<@(_outputs)',
-                '--isolate', 'sync_integration_tests.isolate',
-              ],
-            },
+          'sources': [
+            'sync_integration_tests.isolate',
           ],
         },
       ],

third_party/cacheinvalidation/README.chromium
@@ -10,4 +10,4 @@ Description:
 This is the API to talk to the Google Cache Invalidation service.
 
 Local Modifications:
-None
+None.

third_party/cacheinvalidation/cacheinvalidation.gyp
@@ -157,29 +157,11 @@
             'cacheinvalidation_unittests',
           ],
           'includes': [
+            '../../build/isolate.gypi',
             'cacheinvalidation_unittests.isolate',
           ],
-          'actions': [
-            {
-              'action_name': 'isolate',
-              'inputs': [
-                'cacheinvalidation_unittests.isolate',
-                '<@(isolate_dependency_tracked)',
-              ],
-              'outputs': [
-                '<(PRODUCT_DIR)/cacheinvalidation_unittests.isolated',
-              ],
-              'action': [
-                'python',
-                '../../tools/swarm_client/isolate.py',
-                '<(test_isolation_mode)',
-                '--outdir', '<(test_isolation_outdir)',
-                '--variable', 'PRODUCT_DIR', '<(PRODUCT_DIR)',
-                '--variable', 'OS', '<(OS)',
-                '--result', '<@(_outputs)',
-                '--isolate', 'cacheinvalidation_unittests.isolate',
-              ],
-            },
+          'sources': [
+            'cacheinvalidation_unittests.isolate',
           ],
         },
       ],

commit 0653e056b99f984ffc8e3f2de15c8e7998e53f0a

Fix isolate steps on Windows; regressed in r163144
'python' must be explicitly stated on the command line for it to work on
Windows. Otherwise a prompt appears to select the tool to open 'isolate.py'
with.

TBR=csharp@chromium.org
NOTRY=true
BUG=


Review URL: https://chromiumcodereview.appspot.com/11264009

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163832 0039d316-1c4b-4281-b951-d872f2087c98

build/isolate.gypi
@@ -47,6 +47,7 @@
         '<(PRODUCT_DIR)/<(RULE_INPUT_ROOT).isolated',
       ],
       'action': [
+        'python',
         '<(DEPTH)/tools/swarm_client/isolate.py',
         '<(test_isolation_mode)',
         '--outdir', '<(test_isolation_outdir)',