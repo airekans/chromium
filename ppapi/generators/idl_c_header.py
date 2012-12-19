airekans/chromium

commit 6faeb209cd4f0a2f09398361a800124f7937f727

Added test and fixed bugs with release dependency
Fixed 'visited' bug where sibling were poluting the visit set, preventing dependency expantion.
Fixed release list generation where a node's typeref releases were not constrained by the node's min/max.
Added generator unit tests to PRESUBMIT when changes to generator are detected.
Added test for release dependency when a child uses different releases than the parent.




Fix release dependencies and tests.
R=sehr@chromium.org
BUG=157017


Review URL: https://chromiumcodereview.appspot.com/11419173

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171499 0039d316-1c4b-4281-b951-d872f2087c98

ppapi/generators/idl_c_header.py
@@ -23,13 +23,17 @@ Option('dstroot', 'Base directory of output', default=os.path.join('..', 'c'))
 Option('guard', 'Include guard prefix', default=os.path.join('ppapi', 'c'))
 
 
-def GetOutFileName(filenode, relpath=None, prefix=None):
+def GetPathFromNode(filenode, relpath=None, ext=None):
   path, name = os.path.split(filenode.GetProperty('NAME'))
-  name = os.path.splitext(name)[0] + '.h'
-  if prefix: name = '%s%s' % (prefix, name)
+  if ext: name = os.path.splitext(name)[0] + ext
   if path: name = os.path.join(path, name)
   if relpath: name = os.path.join(relpath, name)
+  name = os.path.normpath(name)
   return name
+
+
+def GetHeaderFromNode(filenode, relpath=None):
+  return GetPathFromNode(filenode, relpath, ext='.h')
 
 
 def WriteGroupMarker(out, node, last_group):
@@ -108,7 +112,7 @@ class HGen(GeneratorByFile):
     Generator.__init__(self, 'C Header', 'cgen', 'Generate the C headers.')
 
   def GenerateFile(self, filenode, releases, options):
-    savename = GetOutFileName(filenode, GetOption('dstroot'))
+    savename = GetHeaderFromNode(filenode, GetOption('dstroot'))
     my_min, my_max = filenode.GetMinMax(releases)
     if my_min > releases[-1] or my_max < releases[0]:
       if os.path.isfile(savename):
@@ -126,7 +130,7 @@ class HGen(GeneratorByFile):
     __pychecker__ = 'unusednames=options'
     cgen = CGen()
     gpath = GetOption('guard')
-    def_guard = GetOutFileName(filenode, relpath=gpath)
+    def_guard = GetHeaderFromNode(filenode, relpath=gpath)
     def_guard = def_guard.replace(os.sep,'_').replace('.','_').upper() + '_'
 
     cright_node = filenode.GetChildren()[0]
@@ -137,8 +141,7 @@ class HGen(GeneratorByFile):
     out.Write('%s\n' % cgen.Copyright(cright_node))
 
     # Wrap the From ... modified ... comment if it would be >80 characters.
-    from_text = 'From %s' % (
-        filenode.GetProperty('NAME').replace(os.sep,'/'))
+    from_text = 'From %s' % GetPathFromNode(filenode)
     modified_text = 'modified %s.' % (
         filenode.GetProperty('DATETIME'))
     if len(from_text) + len(modified_text) < 74:
@@ -158,7 +161,7 @@ class HGen(GeneratorByFile):
       depfile = dep.GetProperty('FILE')
       if depfile:
         includes.add(depfile)
-    includes = [GetOutFileName(
+    includes = [GetHeaderFromNode(
         include, relpath=gpath).replace(os.sep, '/') for include in includes]
     includes.append('ppapi/c/pp_macros.h')
 
@@ -167,7 +170,8 @@ class HGen(GeneratorByFile):
       includes.append('ppapi/c/pp_stdint.h')
 
     includes = sorted(set(includes))
-    cur_include = GetOutFileName(filenode, relpath=gpath).replace(os.sep, '/')
+    cur_include = GetHeaderFromNode(filenode,
+                                    relpath=gpath).replace(os.sep, '/')
     for include in includes:
       if include == cur_include: continue
       out.Write('#include "%s"\n' % include)
@@ -209,14 +213,14 @@ class HGen(GeneratorByFile):
   def GenerateTail(self, out, filenode, releases, options):
     __pychecker__ = 'unusednames=options,releases'
     gpath = GetOption('guard')
-    def_guard = GetOutFileName(filenode, relpath=gpath)
+    def_guard = GetPathFromNode(filenode, relpath=gpath, ext='.h')
     def_guard = def_guard.replace(os.sep,'_').replace('.','_').upper() + '_'
     out.Write('#endif  /* %s */\n\n' % def_guard)
 
 
 hgen = HGen()
 
-def Main(args):
+def main(args):
   # Default invocation will verify the golden files are unchanged.
   failed = 0
   if not args:
@@ -249,5 +253,5 @@ def Main(args):
   return failed
 
 if __name__ == '__main__':
-  sys.exit(Main(sys.argv[1:]))
+  sys.exit(main(sys.argv[1:]))
 

ppapi/PRESUBMIT.py
@@ -31,10 +31,10 @@ def RunUnittests(input_api, output_api):
     if name_parts[0:2] == ['ppapi', 'generators']:
       generator_files.append(filename)
   if generator_files != []:
-    cmd = [ sys.executable, 'idl_gen_pnacl.py', '--wnone', '--test']
+    cmd = [ sys.executable, 'idl_tests.py']
     ppapi_dir = input_api.PresubmitLocalPath()
     results.extend(RunCmdAndCheck(cmd,
-                                  'PPAPI IDL Pnacl unittest failed.',
+                                  'PPAPI IDL unittests failed.',
                                   output_api,
                                   os.path.join(ppapi_dir, 'generators')))
   return results

ppapi/generators/idl_c_proto.py
@@ -449,7 +449,7 @@ class CGen(object):
 
     # TODO(noelallen) : Bug 157017 finish multiversion support
     if len(build_list) != 1:
-      node.Error('Can not support multiple versions of node.')
+      node.Error('Can not support multiple versions of node: %s' % build_list)
     assert len(build_list) == 1
 
     out = 'typedef %s;\n' % self.GetSignature(node, build_list[0], 'return',
@@ -671,7 +671,8 @@ def TestFile(filenode):
     outstr = CleanString(outstr)
 
     if instr != outstr:
-      ErrOut.Log('Failed match of\n>>%s<<\n>>%s<<\nto:' % (instr, outstr))
+      ErrOut.Log('Failed match of\n>>%s<<\nto:\n>>%s<<\nFor:\n' %
+                 (instr, outstr))
       node.Dump(1, comments=True)
       errors += 1
   return errors
@@ -701,7 +702,7 @@ def TestFiles(filenames):
     InfoOut.Log('Passed generator test.')
   return total_errs
 
-def Main(args):
+def main(args):
   filenames = ParseOptions(args)
   if GetOption('test'):
     return TestFiles(filenames)
@@ -716,5 +717,5 @@ def Main(args):
 
 
 if __name__ == '__main__':
-  sys.exit(Main(sys.argv[1:]))
+  sys.exit(main(sys.argv[1:]))
 

ppapi/generators/idl_log.py
@@ -12,6 +12,7 @@
 #
 import sys
 
+
 class IDLLog(object):
   def __init__(self, name, out):
     if name:

ppapi/generators/idl_node.py
@@ -322,7 +322,8 @@ class IDLNode(IDLRelease):
     return self.releases
 
 
-  def _GetReleaseList(self, releases, visited=set()):
+  def _GetReleaseList(self, releases, visited=None):
+    visited = visited or set()
     if not self.releases:
       # If we are unversionable, then return first available release
       if self.IsA('Comment', 'Copyright', 'Label'):
@@ -344,14 +345,18 @@ class IDLNode(IDLRelease):
 
       visited |= set([self])
 
-      # Files inherit all thier releases from items in the file
+      # Files inherit all their releases from items in the file
       if self.IsA('AST', 'File'):
         my_releases = set()
 
       # Visit all children
       child_releases = set()
+
+      # Exclude sibling results from parent visited set
+      cur_visits = visited
+
       for child in self.children:
-        child_releases |= set(child._GetReleaseList(releases, visited))
+        child_releases |= set(child._GetReleaseList(releases, cur_visits))
         visited |= set(child_releases)
 
       # Visit my type
@@ -359,8 +364,7 @@ class IDLNode(IDLRelease):
       if self.typelist:
         type_list = self.typelist.GetReleases()
         for typenode in type_list:
-          type_releases |= set(typenode._GetReleaseList(releases, visited))
-          visited |= set(type_releases)
+          type_releases |= set(typenode._GetReleaseList(releases, cur_visits))
 
         type_release_list = sorted(type_releases)
         if my_min < type_release_list[0]:
@@ -368,12 +372,11 @@ class IDLNode(IDLRelease):
           self.Error('requires %s in %s which is undefined at %s.' % (
               type_node, type_node.filename, my_min))
 
-      for rel in child_releases:
+      for rel in child_releases | type_releases:
         if rel >= my_min and rel <= my_max:
           my_releases |= set([rel])
 
       self.releases = sorted(my_releases)
-
     return self.releases
 
   def GetReleaseList(self):

ppapi/generators/idl_parser.py
@@ -1130,22 +1130,15 @@ def FindVersionError(releases, node):
   if node.IsA('Interface', 'Struct'):
     comment_list = []
     comment = node.GetOneOf('Comment')
-    if comment:
-      print comment.GetName()
     if comment and comment.GetName()[:4] == 'REL:':
       comment_list = comment.GetName()[5:].strip().split(' ')
-      print comment_list
 
-    if len(comment_list) != len(releases):
-      node.Error("Mismatch size of releases: %s vs %s." % (
-          comment_list, releases))
+    first_list = [node.first_release[rel] for rel in releases]
+    first_list = sorted(set(first_list))
+    if first_list != comment_list:
+      node.Error("Mismatch in releases: %s vs %s." % (
+          comment_list, first_list))
       err_cnt += 1
-    else:
-      first_list = [node.first_release[rel] for rel in releases]
-      if first_list != comment_list:
-        node.Error("Mismatch in releases: %s vs %s." % (
-            comment_list, first_list))
-        err_cnt += 1
 
   for child in node.GetChildren():
     err_cnt += FindVersionError(releases, child)

ppapi/generators/idl_tests.py
@@ -1,0 +1,41 @@
+#!/usr/bin/env python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+""" Test runner for IDL Generator changes """
+
+import subprocess
+import sys
+
+def TestIDL(testname, args):
+  print '\nRunning unit tests for %s.' % testname
+  try:
+    args = [sys.executable, testname] + args
+    subprocess.check_call(args)
+    return 0
+  except subprocess.CalledProcessError as err:
+    print 'Failed with %s.' % str(err)
+    return 1
+
+def main(args):
+  errors = 0
+  errors += TestIDL('idl_lexer.py', ['--test'])
+  assert errors == 0
+  errors += TestIDL('idl_parser.py', ['--test'])
+  assert errors == 0
+  errors += TestIDL('idl_c_header.py', [])
+  assert errors == 0
+  errors += TestIDL('idl_c_proto.py', ['--wnone', '--test'])
+  assert errors == 0
+  errors += TestIDL('idl_gen_pnacl.py', ['--wnone', '--test'])
+  assert errors == 0
+
+  if errors:
+    print '\nFailed tests.'
+  return errors
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
+

ppapi/generators/test_cgen/enum_typedef.h
@@ -4,7 +4,7 @@
  * found in the LICENSE file.
  */
 
-/* From test_cgen/enum_typedef.idl modified Mon Aug 22 15:15:49 2011. */
+/* From test_cgen/enum_typedef.idl modified Wed Dec  5 13:08:05 2012. */
 
 #ifndef PPAPI_C_TEST_CGEN_ENUM_TYPEDEF_H_
 #define PPAPI_C_TEST_CGEN_ENUM_TYPEDEF_H_
@@ -41,11 +41,11 @@ typedef enum {
 /* typedef int32_t i; */
 typedef int32_t i;
 
-/* typedef int32_t i2[2]; */
-typedef int32_t i2[2];
+/* typedef int32_t i2[3]; */
+typedef int32_t i2[3];
 
-/* typedef int32_t (*i_func)(); */
-typedef int32_t (*i_func)();
+/* typedef int32_t (*i_func)(void); */
+typedef int32_t (*i_func)(void);
 
 /* typedef int32_t (*i_func_i)(int32_t i); */
 typedef int32_t (*i_func_i)(int32_t i);

commit 02d507211effabe7da9dbc789b25bb0e5d65679e

idl_c_header: Use "/" as path separator in "From".
Generating C headers on windows changes the direction of the slash,
rewriting every C header when generator.py is run. This appears to be a
regression in commit 6faeb209.

BUG=


Review URL: https://chromiumcodereview.appspot.com/11637004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173941 0039d316-1c4b-4281-b951-d872f2087c98

ppapi/generators/idl_c_header.py
@@ -177,7 +177,7 @@ class HGen(GeneratorByFile):
     out.Write('%s\n' % cgen.Copyright(cright_node))
 
     # Wrap the From ... modified ... comment if it would be >80 characters.
-    from_text = 'From %s' % GetPathFromNode(filenode)
+    from_text = 'From %s' % GetPathFromNode(filenode).replace(os.sep, '/')
     modified_text = 'modified %s.' % (
         filenode.GetProperty('DATETIME'))
     if len(from_text) + len(modified_text) < 74: