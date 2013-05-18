airekans/chromium

commit 3ba15a2031963e2aac3021f966dc2ae9de83ddb0

Make app windows use native frames on Windows.
Also make native frames support min and max size.

BUG=225916
R=ben@chromium.org

Review URL: https://chromiumcodereview.appspot.com/14024009

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198375 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/extensions/native_app_window_views.cc
@@ -152,7 +152,7 @@ void NativeAppWindowViews::InitializeDefaultWindow(
     const ShellWindow::CreateParams& create_params) {
   views::Widget::InitParams init_params(views::Widget::InitParams::TYPE_WINDOW);
   init_params.delegate = this;
-  init_params.remove_standard_frame = true;
+  init_params.remove_standard_frame = ShouldUseChromeStyleFrame();
   init_params.use_system_default_icon = true;
   // TODO(erg): Conceptually, these are toplevel windows, but we theoretically
   // could plumb context through to here in some cases.
@@ -407,35 +407,37 @@ void NativeAppWindowViews::OnViewWasResized() {
   // TODO(jeremya): this doesn't seem like a terribly elegant way to keep the
   // window shape in sync.
 #if defined(OS_WIN) && !defined(USE_AURA)
-  // Set the window shape of the RWHV.
   DCHECK(window_);
   DCHECK(web_view_);
   gfx::Size sz = web_view_->size();
   int height = sz.height(), width = sz.width();
-  int radius = 1;
-  gfx::Path path;
-  if (window_->IsMaximized() || window_->IsFullscreen()) {
-    // Don't round the corners when the window is maximized or fullscreen.
-    path.addRect(0, 0, width, height);
-  } else {
-    if (frameless_) {
-      path.moveTo(0, radius);
-      path.lineTo(radius, 0);
-      path.lineTo(width - radius, 0);
-      path.lineTo(width, radius);
+  if (ShouldUseChromeStyleFrame()) {
+    // Set the window shape of the RWHV.
+    const int kCornerRadius = 1;
+    gfx::Path path;
+    if (window_->IsMaximized() || window_->IsFullscreen()) {
+      // Don't round the corners when the window is maximized or fullscreen.
+      path.addRect(0, 0, width, height);
     } else {
-      // Don't round the top corners in chrome-style frame mode.
-      path.moveTo(0, 0);
-      path.lineTo(width, 0);
+      if (frameless_) {
+        path.moveTo(0, kCornerRadius);
+        path.lineTo(kCornerRadius, 0);
+        path.lineTo(width - kCornerRadius, 0);
+        path.lineTo(width, kCornerRadius);
+      } else {
+        // Don't round the top corners in chrome-style frame mode.
+        path.moveTo(0, 0);
+        path.lineTo(width, 0);
+      }
     }
-    path.lineTo(width, height - radius - 1);
-    path.lineTo(width - radius - 1, height);
-    path.lineTo(radius + 1, height);
-    path.lineTo(0, height - radius - 1);
+    path.lineTo(width, height - kCornerRadius - 1);
+    path.lineTo(width - kCornerRadius - 1, height);
+    path.lineTo(kCornerRadius + 1, height);
+    path.lineTo(0, height - kCornerRadius - 1);
     path.close();
+    SetWindowRgn(web_contents()->GetView()->GetNativeView(),
+                 path.CreateNativeRegion(), 1);
   }
-  SetWindowRgn(web_contents()->GetView()->GetNativeView(),
-               path.CreateNativeRegion(), 1);
 
   SkRegion* rgn = new SkRegion;
   if (!window_->IsFullscreen()) {
@@ -458,6 +460,11 @@ void NativeAppWindowViews::OnViewWasResized() {
   FOR_EACH_OBSERVER(WebContentsModalDialogHostObserver,
                     observer_list_,
                     OnPositionRequiresUpdate());
+}
+
+bool NativeAppWindowViews::ShouldUseChromeStyleFrame() const {
+  return !CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kAppsUseNativeFrame) || frameless_;
 }
 
 // WidgetDelegate implementation.
@@ -546,9 +553,12 @@ views::NonClientFrameView* NativeAppWindowViews::CreateNonClientFrameView(
     }
   }
 #endif
-  ShellWindowFrameView* frame_view = new ShellWindowFrameView(this);
-  frame_view->Init(window_);
-  return frame_view;
+  if (ShouldUseChromeStyleFrame()) {
+    ShellWindowFrameView* frame_view = new ShellWindowFrameView(this);
+    frame_view->Init(window_);
+    return frame_view;
+  }
+  return views::WidgetDelegateView::CreateNonClientFrameView(widget);
 }
 
 bool NativeAppWindowViews::ShouldDescendIntoChildForEventHandling(

chrome/app/generated_resources.grd
@@ -6804,6 +6804,12 @@ Keep your key file in a safe place. You will need it to create new versions of y
       <message name="IDS_FLAGS_ENABLE_LOCALLY_MANAGED_USERS_DESCRIPTION" desc="Description for the flag to enable locally managed users.">
         Enables support for user profiles that are managed by another user and can have restrictions applied.
       </message>
+      <message name="IDS_FLAGS_ENABLE_NATIVE_FRAMES_FOR_APPS_NAME" desc="Title for the flag to enable native style frames for apps.">
+        Native style window frames for packaged apps
+      </message>
+      <message name="IDS_FLAGS_ENABLE_NATIVE_FRAMES_FOR_APPS_DESCRIPTION" desc="Description for the flag to enable native style frames for apps.">
+        Use native-style window decorations for packaged app windows.
+      </message>
       <message name="IDS_FLAGS_ENABLE_WEBP_IN_ACCEPT_HEADER_NAME" desc="Title for the flag to enable 'image/webp' in accept header.">
         Enable 'image/webp' accept header
       </message>

chrome/browser/about_flags.cc
@@ -1445,6 +1445,13 @@ const Experiment kExperiments[] = {
     kOsAll,
     SINGLE_VALUE_TYPE(switches::kEnableWebPInAcceptHeader)
   },
+  {
+    "apps-use-native-frame",
+    IDS_FLAGS_ENABLE_NATIVE_FRAMES_FOR_APPS_NAME,
+    IDS_FLAGS_ENABLE_NATIVE_FRAMES_FOR_APPS_DESCRIPTION,
+    kOsWin,
+    SINGLE_VALUE_TYPE(switches::kAppsUseNativeFrame)
+  },
 };
 
 const Experiment* experiments = kExperiments;

chrome/browser/ui/views/extensions/native_app_window_views.h
@@ -54,6 +54,8 @@ class NativeAppWindowViews : public NativeAppWindow,
   void InitializePanelWindow(const ShellWindow::CreateParams& create_params);
   void OnViewWasResized();
 
+  bool ShouldUseChromeStyleFrame() const;
+
 #if defined(OS_WIN)
   void OnShortcutInfoLoaded(
       const ShellIntegration::ShortcutInfo& shortcut_info);

chrome/common/chrome_switches.cc
@@ -108,6 +108,9 @@ const char kAppsNewInstallBubble[]          = "apps-new-install-bubble";
 // Disable throbber for extension apps.
 const char kAppsNoThrob[]                   = "apps-no-throb";
 
+// Experimental native frame support for packaged apps.
+const char kAppsUseNativeFrame[]            = "apps-use-native-frame";
+
 // Whitelist of servers that Negotiate will generate delegated Kerberos tickets
 // for.
 const char kAuthNegotiateDelegateWhitelist[] =

chrome/common/chrome_switches.h
@@ -48,6 +48,7 @@ extern const char kAppModeAuthCode[];
 extern const char kAppModeOAuth2Token[];
 extern const char kAppsNewInstallBubble[];
 extern const char kAppsNoThrob[];
+extern const char kAppsUseNativeFrame[];
 extern const char kAuthNegotiateDelegateWhitelist[];
 extern const char kAuthSchemes[];
 extern const char kAuthServerWhitelist[];

ui/views/window/custom_frame_view.cc
@@ -239,6 +239,20 @@ gfx::Size CustomFrameView::GetPreferredSize() {
       gfx::Rect(frame_->client_view()->GetPreferredSize())).size();
 }
 
+gfx::Size CustomFrameView::GetMinimumSize() {
+  return frame_->non_client_view()->GetWindowBoundsForClientBounds(
+      gfx::Rect(frame_->client_view()->GetMinimumSize())).size();
+}
+
+gfx::Size CustomFrameView::GetMaximumSize() {
+  gfx::Size max_size = frame_->client_view()->GetMaximumSize();
+  gfx::Size converted_size =
+      frame_->non_client_view()->GetWindowBoundsForClientBounds(
+          gfx::Rect(max_size)).size();
+  return gfx::Size(max_size.width() == 0 ? 0 : converted_size.width(),
+                   max_size.height() == 0 ? 0 : converted_size.height());
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // CustomFrameView, ButtonListener implementation:
 

ui/views/window/custom_frame_view.h
@@ -52,6 +52,8 @@ class CustomFrameView : public NonClientFrameView,
   virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
   virtual void Layout() OVERRIDE;
   virtual gfx::Size GetPreferredSize() OVERRIDE;
+  virtual gfx::Size GetMinimumSize() OVERRIDE;
+  virtual gfx::Size GetMaximumSize() OVERRIDE;
 
   // Overridden from ButtonListener:
   virtual void ButtonPressed(Button* sender, const ui::Event& event) OVERRIDE;

commit 04ca656141c0086551c76a69083e16c3410089a3

Fix broken app window frame rendering on Windows
This bug was introduced by r198375, causing fullscreen or
maximized app windows to be clobbered by stuff that shouldn't
be rendered.

BUG=238935

Review URL: https://chromiumcodereview.appspot.com/15300009

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200975 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/extensions/native_app_window_views.cc
@@ -453,12 +453,12 @@ void NativeAppWindowViews::OnViewWasResized() {
         path.moveTo(0, 0);
         path.lineTo(width, 0);
       }
+      path.lineTo(width, height - kCornerRadius - 1);
+      path.lineTo(width - kCornerRadius - 1, height);
+      path.lineTo(kCornerRadius + 1, height);
+      path.lineTo(0, height - kCornerRadius - 1);
+      path.close();
     }
-    path.lineTo(width, height - kCornerRadius - 1);
-    path.lineTo(width - kCornerRadius - 1, height);
-    path.lineTo(kCornerRadius + 1, height);
-    path.lineTo(0, height - kCornerRadius - 1);
-    path.close();
     SetWindowRgn(web_contents()->GetView()->GetNativeView(),
                  path.CreateNativeRegion(), 1);
   }